f8e069276f466a714d06421bd72f1e86745617e8
Scott Humphries <git@sscotth.io>
2016-07-25 11:53:29 -0500

    foods data with xhr

--- src/script.js
+++ src/script.js
@@ -1,1 +1,23 @@
-console.log('XHR 101')
+var tbody = document.querySelector('tbody')
+
+var xhr = new XMLHttpRequest()
+xhr.open('GET', 'data.json')
+xhr.addEventListener('load', didLoadData)
+xhr.send()
+
+function didLoadData () {
+  var foods = JSON.parse(xhr.responseText).foods
+
+  foods.forEach(function (food) {
+    tbody.innerHTML += `
+      <tr>
+        <td>${food.name}</td>
+        <td>${food.qty}</td>
+      </tr>`
+  })
+}
+
+
+
+
+
