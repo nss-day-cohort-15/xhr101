8749706f84529e14c4cc4af6bf5271819f617d83
Scott Humphries <git@sscotth.io>
2016-07-26 10:19:04 -0500

    SYNC Callbacks

--- src/script.js
+++ src/script.js
@@ -18,7 +18,20 @@ function didLoadData () {
   })
 }
 
+// SYNCHRONOUS CALLBACKS
+// DIY SYNC forEach function (ARRAY: array, FUNCTION: callback)
+// callback function (OBJECT: currentValue)
 
+function forEach (array, callback) {
+  for (var i = 0; i < array.length; i++) {
+    callback(array[i])
+  }
+}
 
+// USAGE:
 
+var foods = ['apple', 'banana', 'carrot']
 
+forEach(foods, function (food) {
+  console.log(food)
+})
